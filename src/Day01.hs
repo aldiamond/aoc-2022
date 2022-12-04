1a01bdcebb4b4435e05f3934c810b25f2624a919
Alex Diamond <alex.diamond@sendle.com>
2022-12-05 10:34:46 +1100

    No more exceptions

--- src/Day01.hs
+++ src/Day01.hs
@@ -7,11 +7,11 @@ import qualified Data.Text as T
 import qualified Data.Text.IO as TIO (readFile)
 import qualified Data.Text.Read as T
 
-parseCalories :: T.Text -> [[Integer]]
-parseCalories = fmap (fmap parseInt . T.lines) . T.splitOn (T.pack "\n\n")
+parseCalories :: T.Text -> Either String [[Integer]]
+parseCalories = mapM (mapM parseInt . T.lines) . T.splitOn (T.pack "\n\n")
   where
-    parseInt :: T.Text -> Integer
-    parseInt = either error fst . T.decimal
+    parseInt :: T.Text -> Either String Integer
+    parseInt = fmap fst . T.decimal
 
 maxCalories :: [[Integer]] -> Integer
 maxCalories = foldl max (0 :: Integer) . map sum
@@ -23,7 +23,12 @@ solution :: IO ()
 solution = do
   input <- TIO.readFile "data/day01.txt"
   let calories = parseCalories input
-  print "Day 01: Max Calories"
-  print $ maxCalories calories
-  print "Day 01 (prt2): sum top 3 elf calories"
-  print $ sum . top 3 $ calories
+  case calories of
+    Right cals -> do
+      print "Day 01: Max Calories"
+      print $ maxCalories cals
+      print "Day 01 (prt2): sum top 3 elf calories"
+      print $ sum . top 3 $ cals
+    Left err -> do
+      print "Could not parse input"
+      print err
